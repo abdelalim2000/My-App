3304f2076480c6b7fa77ffeeee214883ccf675e7
Mohamed Ahmed <mohamed.abdelalim2000@gmail.com>
2020-01-31 22:51:56 +0200

    Add 1 redux state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,49 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setSearchField } from "../actions";
 import CardArray from "../components/card_component/CardArray";
 import SearchBox from "../components/searchbox_component/SearchBox";
-import { robots } from "../components/card_component/robots";
 import "./app.scss";
 import Scroll from "../components/Scroll/Scroll";
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: event => dispatch(setSearchField(event.target.value))
+  };
+};
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robot: [],
-      searchField: ""
+      robot: []
     };
   }
 
   componentDidMount() {
-    this.setState({ robot: robots });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robot: users });
+      });
   }
 
-  handelSearch = e => {
-    this.setState({ searchField: e.target.value });
-  };
-
   render() {
-    const { robot, searchField } = this.state;
+    const { robot } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robot.filter(robots => {
       return robots.name.toLowerCase().includes(searchField.toLowerCase());
     });
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.handelSearch} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardArray robots={filteredRobots} />
         </Scroll>
@@ -39,4 +52,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
 import App from "./containers/App";
 import "tachyons";
+import { searchRobots } from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
